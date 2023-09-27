3605a6b45a75947e72e28f2a3c3cad58e0444ebc
wharry <devwharry@gmail.com>
2023-09-26 22:24:17 -0300

    meu primeiro projeto java

---
+++ meu-primeiro-projeto-java-github/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Deus é amor!");
+        System.out.println("Primeiro projeto java");
+        System.out.println("Connectando o projeto ao Github");
+    }
+}
