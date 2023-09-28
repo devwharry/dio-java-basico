f8e208b064a78376d0cb1d1cf18089ff2254234d
wharry <devwharry@gmail.com>
2023-09-27 23:30:11 -0300

    Tipos e variaveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,13 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Eu sou jogador!");
+        double salarioMinimo = 2500.33;
+
+        float pi = 3.14f;
+        short numeroCurto = 1;
+        int numeroNormal = 100;
+        short numeroCurtoDois = (short) numeroNormal;
+        final double CPF = 123456789-00;
+        System.out.println("valor de PI " + pi);
+    }
+}
